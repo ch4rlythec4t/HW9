005485d4e0a1a0f0c1bdef55f781150e405b24f1
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-09-07 20:08:02 +0300

    Task 66 done

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static int WriteSumNums(int num1, int num2)
+{
+    if (num1 == num2) return num2;
+    else return num1 + WriteSumNums(num1 + 1, num2);
+}
+
+Console.Write("Введите число M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(WriteSumNums(num1: numM, num2: numN));
