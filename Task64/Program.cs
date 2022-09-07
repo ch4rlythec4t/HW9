24dc927a54ec424e273ca558997bed8b95cefe9c
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-09-07 19:40:37 +0300

    Task 64 done

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void WriteNums(int num1, int num2)
+{
+    if (num1 <= num2)
+    {
+        Console.Write(num1 + " ");
+        num1++;
+        WriteNums(num1, num2);
+    }
+}
+
+Console.Write("Введите число M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+WriteNums(num1: numM, num2: numN);
+
