9ec26cb0dd824fbf45c979a80eb33445d7d67e95
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-09-08 19:29:29 +0300

    Task 68 done

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static int AkkermanFunction(int num1, int num2)
+{
+    if (num1 == 0) return num2 + 1;
+    else
+        if ((num1 != 0) && (num2 == 0)) return AkkermanFunction(num1 - 1, 1);
+        else return AkkermanFunction(num1 - 1, AkkermanFunction(num1, num2 - 1));
+}
+
+Console.Write("Введите число M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(AkkermanFunction(num1: numM, num2: numN));
